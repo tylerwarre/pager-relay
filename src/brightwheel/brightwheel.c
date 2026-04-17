6cc40118c23660abf88ed478d56b552366ffea7a
tylerwarre <tyler@warrens.one>
2026-04-17 14:27:07 -0600

    Successfully parsing brighwheel message response

--- src/brightwheel/brightwheel.c
+++ src/brightwheel/brightwheel.c
@@ -2,6 +2,7 @@
 #include <string.h>
 
 #include <curl/curl.h>
+#include <json-c/json.h>
 
 #include "brightwheel.h"
 #include "http.h"
@@ -11,6 +12,7 @@ struct json_object* bright_get_msgs(BrightwheelSettings *s) {
     CURLcode errno = CURLE_OK;
     HttpResponse resp = {0};
     char *cookies = NULL;
+    struct json_object *j_msgs = NULL;
     
     if ((curl = curl_easy_init()) == NULL) {
         fprintf(stderr, "[%s] Unable to initalize curl for request\n", __func__);
@@ -48,8 +50,11 @@ struct json_object* bright_get_msgs(BrightwheelSettings *s) {
             fprintf(stderr, "[%s] Unable to process curl request: %s\n", __func__, curl_easy_strerror(errno));
             break;
         }
-        // TODO: Implement parsing as json
-        printf("response: %s\n", resp.data);
+
+        if ((j_msgs = bright_parse_msgs(s, resp.data)) == NULL) {
+            break;
+        }
+
         break;
     }
 
@@ -65,5 +70,25 @@ struct json_object* bright_get_msgs(BrightwheelSettings *s) {
 
     curl_easy_cleanup(curl);
 
-    return NULL;
+    return j_msgs;
+}
+
+static struct json_object* bright_parse_msgs(BrightwheelSettings *s, char *msgs) {
+    struct json_object *obj = NULL;
+
+    #ifndef NDEBUG
+        enum json_tokener_error err;
+        obj = json_tokener_parse_verbose(msgs, &err);
+        if (err != json_tokener_success) {
+            fprintf(stderr, "[%s] Unable to parse json_string:\n\tErr: %s\n\tString: %s\n", __func__, json_tokener_error_desc(err), msgs);
+            return NULL;
+        }
+    #else
+        if ((obj = json_tokener_parse(msgs)) == NULL) {
+            fprintf(stderr, "[%s] Unable to parse json_string:\n\tString: %s\n", __func__, msgs);
+            return NULL;
+        }
+    #endif
+
+    return obj;
 }
--- src/pager_relay.c
+++ src/pager_relay.c
@@ -12,6 +12,7 @@ int main() {
     int ret = E_SUCCESS;
     RelaySettings *s = NULL;
     CURLcode c_errno = CURLE_OK;
+    struct json_object *msgs = NULL;
 
     if ((s = settings_new()) == NULL) {
         return E_OUTOFMEMORY;
@@ -25,10 +26,26 @@ int main() {
         fprintf(stderr, "Unable to initalize curl: %s\n", curl_easy_strerror(c_errno));
         return (int)c_errno;
     }
-    
-    printf("token: %s", s->brightwheel->token);
 
-    bright_get_msgs(s->brightwheel);
+    // TODO: implement a loop from here down until the free functions
+    
+    // TODO: update so that bright_get_msgs() returns an int
+    if ((msgs = bright_get_msgs(s->brightwheel)) == NULL) {
+        return EXIT_FAILURE;
+    }
+
+    int count = 0;
+    struct json_object *obj;
+    if(json_object_object_get_ex(msgs, "count", &obj) == false) {
+        return EXIT_FAILURE;
+    }
+    if((count = json_object_get_int(obj)) == 0) {
+        return EXIT_FAILURE;
+    }
+
+    printf("Message Count: %d\n", count);
+
+    json_object_put(msgs);
 
     settings_free(s);
 
