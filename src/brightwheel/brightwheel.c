ee9d5230da83f3407bf7c3e686b21b8a551dc1f6
tylerwarre <tyler@warrens.one>
2026-04-20 22:37:31 -0600

    Fixed memory leak while parsing json response from brightwheel

--- src/brightwheel/brightwheel.c
+++ src/brightwheel/brightwheel.c
@@ -15,6 +15,7 @@ int bright_get_msgs(BrightwheelSettings *s, struct json_object **msgs) {
     CURLcode ret = E_SUCCESS;
     HttpResponse resp = {0};
     char *cookies = NULL;
+    struct json_object *j_reponse = NULL;
     
     if ((curl = curl_easy_init()) == NULL) {
         fprintf(stderr, "[%s] Unable to initalize curl for request\n", __func__);
@@ -52,18 +53,24 @@ int bright_get_msgs(BrightwheelSettings *s, struct json_object **msgs) {
             fprintf(stderr, "[%s] Unable to process curl request: %s\n", __func__, curl_easy_strerror(ret));
             break;
         }
+    
+        #ifndef NDEBUG
+            printf("Response: %s\n", resp.data);
+        #endif
 
-        if ((*msgs = bright_parse_msgs(s, resp.data)) == NULL) {
+        if ((j_reponse = bright_parse_msgs(s, resp.data)) == NULL) {
             fprintf(stderr, "[%s] Unable to parse json response\n", __func__);
             ret = E_JSON_PARSE;
             break;
         }
 
-        if (json_object_object_get_ex(*msgs, "results", msgs) == false) {
+        if (json_object_object_get_ex(j_reponse, "results", msgs) == false) {
             fprintf(stderr, "[%s] Unable to get results array from response\n", __func__);
             ret = E_JSON_ACCESS;
             break;
         }
+
+        json_object_get(*msgs);
 
         break;
     }
@@ -77,6 +84,9 @@ int bright_get_msgs(BrightwheelSettings *s, struct json_object **msgs) {
         free(resp.data);
         resp.data = NULL;
     }
+
+    json_object_object_del(j_reponse, "results");
+    json_object_put(j_reponse);
 
     curl_easy_cleanup(curl);
 
@@ -158,19 +168,19 @@ int bright_get_last_msg(BrightState *state, json_object *msgs, char *msg) {
     return ret;
 }
 
-static struct json_object* bright_parse_msgs(BrightwheelSettings *s, char *msgs) {
+static struct json_object* bright_parse_msgs(BrightwheelSettings *s, char *j_str) {
     struct json_object *obj = NULL;
 
     #ifndef NDEBUG
         enum json_tokener_error err;
-        obj = json_tokener_parse_verbose(msgs, &err);
+        obj = json_tokener_parse_verbose(j_str, &err);
         if (err != json_tokener_success) {
-            fprintf(stderr, "[%s] Unable to parse json_string:\n\tErr: %s\n\tString: %s\n", __func__, json_tokener_error_desc(err), msgs);
+            fprintf(stderr, "[%s] Unable to parse json_string:\n\tErr: %s\n\tString: %s\n", __func__, json_tokener_error_desc(err), j_str);
             return NULL;
         }
     #else
-        if ((obj = json_tokener_parse(msgs)) == NULL) {
-            fprintf(stderr, "[%s] Unable to parse json_string:\n\tString: %s\n", __func__, msgs);
+        if ((obj = json_tokener_parse(j_str)) == NULL) {
+            fprintf(stderr, "[%s] Unable to parse json_string:\n\tString: %s\n", __func__, j_str);
             return NULL;
         }
     #endif
